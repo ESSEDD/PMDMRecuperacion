31ef8f223235c7829c1d0378ae5f461f895ad2fb
usertarb <sergio_010699@hotmail.com>
2021-04-14 17:17:58 +0200

    Burger Activity

--- app/src/main/java/com/example/adivina/PedirAmburguesa.java
+++ app/src/main/java/com/example/adivina/PedirAmburguesa.java
@@ -2,13 +2,27 @@ package com.example.adivina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class PedirAmburguesa extends AppCompatActivity {
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedir_amburguesa);
+
+        videoView = findViewById(R.id.videoView);
+        Uri uri = Uri.parse("https://youtu.be/jN5Q_Po_vjc");
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+
+        videoView.start();
     }
 }
